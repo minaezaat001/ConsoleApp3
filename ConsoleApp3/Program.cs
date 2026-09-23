3fa37e34452668346e608cd05b63210f50680358
Mina001 <minaezaat9@gmail.com>
2026-09-23 21:47:50 +0200

    Create My Project

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+
+            Console.WriteLine("E");
+            Console.WriteLine("FF");
+
+
+        }
+    }
+}
